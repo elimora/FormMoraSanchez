4514a6a118b6ae7cadfcb70269c53595ff8f4835
JASF9 <52477379+JASF9@users.noreply.github.com>
2021-03-02 22:05:41 -0400

    Controladores y rutas añadidas

--- server/models/database.js
+++ server/models/database.js
@@ -1,20 +1,14 @@
-      const {email,password}= req.body;    
+const { Pool } = require('pg')
 
-        //const idUser=  pool.query(`SELECT 'id'  from loggin WHERE 'email'  = '${req.body.email}' `);
-        const idUser= await pool.query(`SELECT id  from loggin WHERE email  = '${req.body.email}' `);
-        //console.log(idUser.rows)SELECT id  from loggin WHERE email  = 'edictaLuzardo' 
-        console.log(idUser.rows); 
-        res.send('ok')
-        if (idUser.rows) {
-            const validPass= await pool.query(`select password  from loggin WHERE email  = '${req.body.email}' `)
-            //SELECT password  from loggin WHERE email  = 'angelicas@hotmail.com' 
-           const {password}= req.body
-            console.log(password)
-        }
-      
+const pool = new Pool({
+    host:'localhost',
+    user :'postgres', 
+    password:'JASF0911', 
+    database:'formularios', 
+    port:'5433'
 
-    } catch (e) {
-        console.log(e); 
-        res.status(500).send('Algo salio mal') 
-    }
-     
+})
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
